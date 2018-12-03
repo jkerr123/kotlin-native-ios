737dff82aae447135b7660f4ff39ee8728b3f438
Jamie Kerr <jamie.kerr@izettle.com>
2018-12-03 09:51:00 +0000

    Add photo taking

--- kotlin/ViewController.swift
+++ kotlin/ViewController.swift
@@ -9,29 +9,56 @@
 import UIKit
 import common2
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+    UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    var imagePickerController: UIImagePickerController!
+
+    @IBAction func onTakePhoto(_ sender: Any) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+
+        guard let takenImage = info[.originalImage] as? UIImage else {
+            fatalError("error capturing image")
+        }
+
+        imageView.image = takenImage
+        imagePickerController.dismiss(animated: true, completion: nil)
+        let pngImage = takenImage.pngData()
+        let base64String = pngImage?.base64EncodedString()
+
+        // TODO pass image to kotlin native that does stuff with iut
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let filter = Filter()
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.font = label.font.withSize(15)
-        let list = ["Monkey", "Tarantula", "Software", "Frijj","Cat", "Hi", "Pi"]
-        let filtered = filter.filterLength(input: list, limit: 5)
-        label.text = "Result is \(filtered))"
-        view.addSubview(label)
+
+//        let filter = Filter()
+//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+//        label.center = CGPoint(x: 160, y: 285)
+//        label.textAlignment = .center
+//        label.font = label.font.withSize(15)
+//        let list = ["Monkey", "Tarantula", "Software", "Frijj","Cat", "Hi", "Pi"]
+//        let filtered = filter.filterLength(input: list, limit: 5)
+//        label.text = "Result is \(filtered))"
+//        view.addSubview(label)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        let platform = Platform()
-        let alert = UIAlertController(title: "Hello", message: "Kotlin Native on \(platform.name())", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+
+//        let platform = Platform()
+//        let alert = UIAlertController(title: "Hello", message: "Kotlin Native on \(platform.name())", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//        self.present(alert, animated: true, completion: nil)
     }
 }
 
